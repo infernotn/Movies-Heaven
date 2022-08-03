ddbcc0a16b92ab06aae07b3f2937eda6383b42e2
fehmi mtira <60454322+infernotn@users.noreply.github.com>
2022-08-03 22:10:43 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { HiSearch } from "react-icons/hi";
 import { MovieCard } from "./MovieCard";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 function App() {
   const api_url = "http://www.omdbapi.com?apikey=c013032f";
@@ -10,6 +11,7 @@ function App() {
     const response = await fetch(`${api_url}&s=${title}`);
     const data = await response.json();
     await setMovies(data.Search);
+    console.log("hi");
     console.log(data.Search);
     console.log(movies);
   };
@@ -24,25 +26,46 @@ function App() {
       </h1>
 
       <div className="w-[60%] md:w-[30%]  mt-5 flex bg-slate-800 bg-opacity-0 focus-within:bg-opacity-50 justify-center items-center rounded-md p-1 opacity-85">
-        <input
-          className="w-[90%] h-10 text-lg pl-2 bg-transparent border-none text-slate-50 text opacity-30 focus-within:text-white  focus-within:text-opacity-100 "
-          placeholder="search for a movie"
-          value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-        />
+        <div className="w-[90%] h-10 flex justify-center items-center relative">
+          <input
+            className="w-full h-full text-lg pl-2 bg-transparent border-none text-slate-50 text opacity-30 focus-within:text-white  focus-within:text-opacity-100 "
+            placeholder="search for a movie"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+              searchMovieas(searchTerm);
+            }}
+          />
+          {
+            <AiFillCloseCircle
+              className={
+                (searchTerm != 0 ? "opacity-100" : "opacity-0") +
+                "   absolute right-2 text-2xl text-slate-50 cursor-pointer hover:scale-x-105"
+              }
+              onClick={() => {
+                setSearchTerm("");
+              }}
+            />
+          }
+        </div>
         <HiSearch
           className="flex w-[10%]  justify-center items-center  text-white   text-2xl hover:cursor-pointer hover:scale-110"
           onClick={() => searchMovieas(searchTerm)}
         />
       </div>
+      {/* Movies Cards */}
       <div className="w-screen mt-14 flex flex-wrap justify-center items-center gap-x-10 gap-y-14 ">
-        {movies?.length > 0
-          ? movies.map((movie) => {
-              return <MovieCard movie={movie} />;
-            })
-          : ""}
+        {movies?.length > 0 ? (
+          movies.map((movie) => {
+            return <MovieCard movie={movie} />;
+          })
+        ) : searchTerm.length > 10 ? (
+          <span className="h-[100%] mt-10 text-3xl text-gray-400 ">
+            404 not found
+          </span>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -3,18 +3,20 @@ import React from "react";
 export const MovieCard = ({ movie }) => {
   console.log(movie.Poster);
   return (
-    <div className="hover:scale-110 hover:z-10 hover:bg-opacity-100 transition-transform relative max-w-xs md:max-w-sm p-1 rounded-lg bg-slate-800 bg-opacity-70 flex flex-col items-center justify-center">
-      <h3 className="w-full absolute top-[1%] left-[3%] text-white">
+    <div className="hover:scale-110 hover:z-10 hover:b          g-opacity-100 transition-transform relative h-[25rem] w-[18rem] max-w-xs md:max-w-sm p-2 rounded-lg bg-slate-800 bg-opacity-70 flex flex-col items-center justify-center">
+      <h3 className="w-full absolute top-[1%] pl-3 text-white bg-slate-800 bg-opacity-70">
         {movie.Year}
       </h3>
-      <img
-        src={
-          movie.Poster !== `N/A`
-            ? movie.Poster
-            : "https://upload.wikimedia.org/wikipedia/commons/6/64/Poster_not_available.jpg"
-        }
-        className="min-w-fit w-full  max-h-sm "
-      />
+      <div className="w-full h-[100%] overflow-hidden flex justify-center items-center">
+        <img
+          src={
+            movie.Poster !== `N/A`
+              ? movie.Poster
+              : "https://upload.wikimedia.org/wikipedia/commons/6/64/Poster_not_available.jpg"
+          }
+          className="  max-h-fit max-w-fit  "
+        />
+      </div>
       <h2 className="text-slate-300 text-m mt-2 ">{movie.Title}</h2>
     </div>
   );
